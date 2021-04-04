db29979f337b96a246fc8d12d0bf1cbb6d03f9ba
NickOwens17 <nickcowens17@gmail.com>
2021-04-04 15:03:47 -0400

    Orchestration in development bg

--- web/src/components/DevelopmentSection.js
+++ web/src/components/DevelopmentSection.js
@@ -8,14 +8,40 @@ const animationProps = {
   transition: { duration: 0.6, ease: "easeOut", delay: 0.4 }
 }
 
+const containerVariants = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+}
+
+const boxVariants = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: 1200 },
+}
+
 const DevelopmentBackground = () => {
   return (
-    <div className={container}>
-      <motion.div {...animationProps} className={[box, box1].join(' ')}></motion.div>
-      <motion.div {...animationProps} className={[box, box2].join(' ')}></motion.div>
-      <motion.div {...animationProps} className={[box, box3].join(' ')}></motion.div>
-      <motion.div {...animationProps} className={[box, box4].join(' ')}></motion.div>
-    </div>
+    <motion.div className={container} 
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+      transition={{duration: 0.6, delay: 0.4}}
+    >
+      <motion.div variants={boxVariants} className={[box, box1].join(' ')}></motion.div>
+      <motion.div variants={boxVariants} className={[box, box2].join(' ')}></motion.div>
+      <motion.div variants={boxVariants} className={[box, box3].join(' ')}></motion.div>
+      <motion.div variants={boxVariants} className={[box, box4].join(' ')}></motion.div>
+    </motion.div>
   )
 }
 
